e729b9a3d671eb75f05c2b6c5a0e5813ffd89b02
Irving Leonard <irvingleonard@gmail.com>
2024-10-17 18:45:22 -0400

    Moving towards a first build

--- python_altinstall_packager/__init__.py
+++ python_altinstall_packager/__init__.py
@@ -3,12 +3,15 @@
 
 """
 
+from atexit import register as atexit_register
 from json import dumps as json_dumps, loads as json_loads
 from logging import getLogger
 from pathlib import Path
+from shutil import rmtree
+from tempfile import mkdtemp
+from urllib.parse import urlparse
 
-from . import _data as DATA
-from .specfile_ import Changelog
+from requests import get as requests_get
 
 try:
 	from jinja2 import Environment as Jinja2Environment
@@ -18,6 +21,9 @@ try:
 	from docker import from_env as docker_from_env
 except ImportError:
 	docker_from_env = None
+
+from . import _data as DATA
+from .specfile_ import Changelog
 
 __version__ = '0.1.0.dev0'
 
@@ -32,16 +38,32 @@ class SpecfileTemplate(dict):
 	TEMPLATE_NAME = 'specfile.jinja'
 	_values_loaded = False
 	
+	def __call__(self, destination_dir=Path.cwd()):
+		"""
+		
+		:param destination_dir:
+		:return:
+		"""
+		
+		pass
+	
 	def __getattr__(self, item):
 		"""
 
 		"""
 		
-		if item == 'requires':
+		if item == 'distro_data':
 			if self.dist not in DATA.distributions:
 				raise ValueError('Missing "{}" distribution in knowledge base'.format(self.dist))
 			value = DATA.distributions[self.dist]
-			value = value['requires'] if 'requires' in value else None
+		elif item == 'python_data':
+			for ver_len in range(len(self['python_version']), -1, -1):
+				python_version = '.'.join(self['python_version'][:ver_len])
+				if not python_version:
+					raise ValueError('Missing "{}" python in knowledge base'.format(python_version))
+				if python_version in DATA.python:
+					value = DATA.python[python_version]
+					break
 		elif item == 'template_file_path':
 			value = Path(__file__).parent / 'data' / self.TEMPLATE_NAME
 			if not value.is_file():
@@ -62,10 +84,8 @@ class SpecfileTemplate(dict):
 		if isinstance(python_version, str):
 			python_version = python_version.split('.')
 		self['python_version'] = python_version
-		if self.requires is not None:
-			self['requires'] = self.requires
-		changelog_file = Path(changelog_file)
-		self['changelog'] = Changelog(json_loads(changelog_file.read_text()))
+		self['package_name'] = 'python{}-altinstall'.format(''.join(python_version[:2]))
+		self.changelog_file = Path(changelog_file)
 	
 	def __str__(self):
 		"""
@@ -76,6 +96,7 @@ class SpecfileTemplate(dict):
 			raise ImportError('The "jinja2" package is required by {}'.format(type(self).__name__))
 		jinja_env = Jinja2Environment()
 		result = jinja_env.from_string(self.template_file_path.read_text())
+		self._load_values()
 		return result.render(self)
 	
 	def _load_values(self):
@@ -84,7 +105,25 @@ class SpecfileTemplate(dict):
 		:return:
 		"""
 		
-		self.update(self.de)
+		if 'build_number' not in self:
+			self['build_number'] = 1
+		if 'requires' in self.distro_data:
+			self['requires'] = self.distro_data['requires']
+		self.update(self.python_data)
+		self['changelog'] = Changelog(json_loads(self.changelog_file.read_text()))
+		
+	def write_file(self, specs_dir=Path.cwd()):
+		"""
+		
+		:param specs_dir:
+		:return:
+		"""
+		
+		specs_dir = Path(specs_dir)
+		specs_dir.mkdir(parents=True, exist_ok=True)
+		file_name = specs_dir / (self['package_name'] + '.spec')
+		file_name.write_text(str(self))
+		return file_name
 
 
 class DockerfileTemplate(dict):
@@ -100,7 +139,6 @@ class DockerfileTemplate(dict):
 
 		"""
 		
-		self.dockerfile = self.root_dir / 'Dockerfile'
 		self.dockerfile.write_text(str(self))
 		return self.dockerfile
 	
@@ -128,6 +166,8 @@ class DockerfileTemplate(dict):
 			value = DATA.distributions[self.dist]
 			if 'dockerfile' in value:
 				self.update(value['dockerfile'])
+		elif item == 'dockerfile':
+			value = self.root_dir / 'Dockerfile'
 		elif item == 'packaging_tool':
 			if 'packaging_tool' not in self.defaults:
 				raise ValueError('Missing "packaging_tool" section for "{}" distribution in knowledge base'.format(self.dist))
@@ -192,11 +232,94 @@ class DockerfileTemplate(dict):
 		run_arguments_.update(run_arguments)
 		return self.client.containers.run(self.TAG_NAME, **run_arguments_)
 	
+	def write_file(self, output_dir=None):
+		"""
+		
+		:return:
+		"""
+		
+		if output_dir is None:
+			dockerfile = self.dockerfile
+		else:
+			dockerfile = Path(output_dir) / 'Dockerfile'
+		return dockerfile.write_text(str(self))
+
 
 class PythonAltinstallPackager:
 	"""
 	
 	"""
+	
+	DOWNLOAD_PATH_TEMPLATE = r'https://www.python.org/ftp/python/{version}/Python-{version}.tgz'
+	
+	def __init__(self, dist_dir=Path.cwd()):
+		"""
+		
+		:param build_dir:
+		"""
+		
+		self._dist_dir = dist_dir
+		# if build_dir is None:
+		# 	self._build_dir = Path(mkdtemp()).absolute()
+		# 	atexit_register(rmtree, build_dir, ignore_errors=True)
+		# else:
+		# 	self._build_dir = Path(build_dir)
+	
+	def build(self, *versions, distributions=None):
+		"""
+		
+		:param versions:
+		:param distributions:
+		:return:
+		"""
+		
+		if distributions is None:
+			distributions = tuple(DATA.distributions.keys())
+		
+		for version in versions:
+			print(version, distributions)
+	
+	def download_tarball(self, version, stream_chunk_size=1048576, destination_dir=None, overwrite=False):
+		"""
+		
+		:param version:
+		:param stream_chunk_size:
+		:param destination_dir:
+		:param overwrite:
+		:return:
+		"""
+		
+		download_url = self.DOWNLOAD_PATH_TEMPLATE.format(version=version)
+		destination_dir = Path.cwd() if destination_dir is None else Path(destination_dir)
+		destination_dir.mkdir(exist_ok=True)
+		local_file = destination_dir / Path(urlparse(download_url).path).name
+		if local_file.exists() and not overwrite:
+			return local_file
+		else:
+			local_file.unlink(missing_ok=True)
+		
+		with requests_get(download_url, stream=True) as source_file:
+			source_file.raise_for_status()
+			with open(local_file, 'wb') as file_obj:
+				for chunk in source_file.iter_content(chunk_size=stream_chunk_size):
+					file_obj.write(chunk)
+		
+		return local_file
+	
+	def prepare_build(self, dist, python_version, output_directory=None):
+		"""
+		
+		:return:
+		"""
+		
+		if output_directory is None:
+			output_directory = self._build_dir
+		else:
+			output_directory = Path(output_directory)
+		output_directory.mkdir(parents=True, exist_ok=True)
+		self.download_tarball(version=python_version, destination_dir=(output_directory / 'SOURCES'))
+		SpecfileTemplate(dist, python_version).write_file(specs_dir=(output_directory / 'SPECS'))
+		DockerfileTemplate(dist, python_version).write_file(output_dir=output_directory)
 	
 	def test(self):
 		
@@ -214,9 +337,10 @@ class PythonAltinstallPackager:
 		# return str(ch)
 		# return ch('Irv LEo <irvleo@example.net>', '4.5.6', '11', ['new testing'])
 		
-		return str(SpecfileTemplate('el9', '3.10.20'))
+		# return str(SpecfileTemplate('el9', '3.10.20'))
 		
 		test_dir = Path.cwd() / 'testing'
+		
 		test_dir.mkdir(exist_ok=True)
 		with DockerfileTemplate('el9', '3.10.20', test_dir, True) as dockerfile:
 			pass
